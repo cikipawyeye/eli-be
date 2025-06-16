f04d10190ca2ac9689e0e395e8b36a05931350a5
zufarf <mzufarfainan@gmail.com>
2025-06-16 21:03:40 +0700

    fix(lang): update token reset message for clarity

--- lang/id/passwords.php
+++ lang/id/passwords.php
@@ -6,6 +6,6 @@ return [
     'reset' => 'Kata sandi Anda sudah direset! Silakan masuk menggunakan kata sandi baru Anda.',
     'sent' => 'Kami sudah mengirim surel yang berisi tautan untuk mereset kata sandi Anda!',
     'throttled' => 'Harap tunggu sebelum mencoba lagi.',
-    'token' => 'Halaman untuk mereset kata sandi ini tidak valid atau sudah kedaluwarsa.',
+    'token' => 'Halaman untuk mereset kata sandi ini sudah kedaluwarsa.',
     'user' => 'Kami tidak dapat menemukan pengguna dengan alamat surel tersebut.',
 ];
